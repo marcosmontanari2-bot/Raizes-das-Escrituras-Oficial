152437c4e7faeb35354a7406aad8d8aa4f5474cb
Marcos Montanaria <marcosmontanari2@gmail.com>
2026-08-16 16:44:08 -0300

    sw corrigindo

--- sw.js
+++ sw.js
@@ -1,11 +1,12 @@
-const CACHE_NAME = 'raizes-escrituras-v3';
+const CACHE_NAME = 'raizes-escrituras-v4';
 const assetsToCache = [
-    '/',
-    'index.html',
-    'offline.html',
-    'style.css',
-    'ui-config.js'
+    './',
+    './index.html',
+    './offline.html',
+    './style.css',
+    './ui-config.js'
 ];
+
 
 self.addEventListener('install', (event) => {
     event.waitUntil(
@@ -15,6 +16,7 @@ self.addEventListener('install', (event) => {
     );
     self.skipWaiting();
 });
+
 
 self.addEventListener('activate', (event) => {
     event.waitUntil(
@@ -30,17 +32,24 @@ self.addEventListener('activate', (event) => {
     );
 });
 
+
 self.addEventListener('fetch', (event) => {
+
+    if (event.request.mode === 'navigate') {
+        event.respondWith(
+            fetch(event.request)
+                .catch(() => {
+                    return caches.match(event.request)
+                        .then(response => response || caches.match('./offline.html'));
+                })
+        );
+        return;
+    }
+
+    
     event.respondWith(
         caches.match(event.request).then((cachedResponse) => {
-            if (cachedResponse) {
-                return cachedResponse;
-            }
-            return fetch(event.request).catch(() => {
-                if (event.request.mode === 'navigate') {
-                    return caches.match('offline.html');
-                }
-            });
+            return cachedResponse || fetch(event.request);
         })
     );
 });
